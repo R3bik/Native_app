e0964e18e87b7837bf39fe6040f2f91123a14644
Rebik Basnet <basnetrebik07@gmail.com>
2025-05-16 12:22:01 +0545

    basic setup

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,22 @@
-import { Text, View } from "react-native";
-
+import { Image, Text, TouchableOpacity, View } from "react-native";
+import { styles } from "../styles/home";
 export default function Index() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+    <View style={styles.container}>
+      <Image
+        source={{
+          uri: "https://cdn.pixabay.com/photo/2019/06/13/13/06/monster-4271569_1280.png",
+        }}
+        style={{
+          width: 100,
+          height: 100,
+          borderRadius: 100,
+        }}
+      />
+      <Text style={styles.text}>Hello World!</Text>
+      <TouchableOpacity onPress={() => alert("Hello World!")}>
+        <Text style={styles.text}>Login</Text>
+      </TouchableOpacity>
     </View>
   );
 }
---
+++ styles/home.js
@@ -0,0 +1,17 @@
+import { StyleSheet } from "react-native";
+
+export const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "black",
+  },
+  text: {
+    fontSize: 20,
+    textAlign: "center",
+    margin: 10,
+    color: "white",
+    fontWeight: "bold",
+  },
+});
